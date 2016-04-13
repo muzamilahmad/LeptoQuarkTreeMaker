c9e03df15b7f683d447930300c2602b053086276
muzamilahmad <muzamilphy@gmail.com>
2016-04-13 13:25:05 +0200

    Muon Producer added

---
+++ Utils/python/MuonProducer_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+MuonProducer = cms.EDProducer('MuonProducer',
+muontag = cms.InputTag('slimmedMuons'),
+#rhotag = cms.InputTag('fixedGridRhoFastjetAll')
+#jettag = cms.InputTag( "slimmedJets")
+)
+
