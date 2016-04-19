623a00b6e3973f6fb68fa624458f279f0c8a6cae
Muzamil Bhat <muzamilphy786@gmail.com>
2016-04-19 09:34:34 -0500

    Muon and Tau producer added

--- Utils/python/MuonProducer_cfi.py
+++ Utils/python/MuonProducer_cfi.py
@@ -4,5 +4,8 @@ MuonProducer = cms.EDProducer('MuonProducer',
 muontag = cms.InputTag('slimmedMuons'),
 #rhotag = cms.InputTag('fixedGridRhoFastjetAll')
 #jettag = cms.InputTag( "slimmedJets")
+tautag = cms.InputTag('slimmedTaus')
+
+
 )
 
